419924ac9e5e5ab94e9324d913dc6a3c1f758441
Andre Pautin <andre.pautin@gmail.com>
2022-01-18 17:23:59 -0800

    removed logo file

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import _ from "lodash";
+import {useState, useEffect} from 'react';
 const BASE_API_URL = "http://jservice.io/api/";
 const NUM_CATEGORIES = 6;
 const NUM_CLUES_PER_CAT = 5;
@@ -6,24 +8,32 @@ const NUM_CLUES_PER_CAT = 5;
 function GameBoard() {
   // state for gameboard for when questions clicked
   // state for if categories were collected
+  const [gotCategories, setGotCategories] = useState(false);
+  const [categoryIds, setCategoryIds] = useState();
   // state for if categoryIds were collected
-  // get categories from api
 
-  let categories = [];
-  let categoryIds = [];
+  // let categories = [];
+  // let categoryIds = [];
   // TODO-- Get category ids
-  async function getCategoryIds() {
-    const response = await axios({
-      url: `${BASE_API_URL}/categories`,
-      method: "GET",
-      params: {count: NUM_CATEGORIES}
-    });
-    console.log(response.data);
-    return response.data;
-  }
-  getCategoryIds();
+  useEffect (
+    function getIds() {
+      async function getCategoryIds() {
+        const response = await axios({
+          url: `${BASE_API_URL}/categories`,
+          method: "GET",
+          params: {count: 100, offset: Math.floor(Math.random() * 1000)}
+        });
+        let sample = _.sampleSize(response.data, NUM_CATEGORIES);
+        setGotCategories(true);
+        setCategoryIds(sample.map(data => data.id));
+        console.log(categoryIds);
+        console.log(sample);
+        return response.data;
+      }
+      getCategoryIds();
+    }, []
+  )
 
-  console.log("rendered GameBoard component");
   return (
     "Gameboard component"
   );
